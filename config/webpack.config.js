b0d78a0d7a0b7b2c5e93e03788990ac5fe9b65c6
Ryo Chikazawa <chikathreesix@gmail.com>
2017-11-01 12:10:13 +0900

    First commit

---
+++ config/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  entry: __dirname + '/../src/javascripts/write.js',
+  output: {
+    path: __dirname + '/../build',
+    filename: 'write.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.styl$/, exclude: /node_modules/, loader: 'style-loader!css-loader!stylus-loader' },
+      { test: /\.png$/, loader: "url-loader?limit=100000" }
+    ]
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false,
+        },
+        output: {
+          comments: false,
+        },
+      }
+    }),
+  ]
+};
